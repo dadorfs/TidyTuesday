215a90eae8f02a8757d9c49e5c29d38c14f51483
ddorfsman <daniel.dorfsman@gmail.com>
2023-08-24 10:03:39 -0400

    update plot

--- 2023-08-22/tidy_tuesday_20230822.R
+++ 2023-08-22/tidy_tuesday_20230822.R
@@ -20,9 +20,10 @@
 
 library(tidytuesdayR)
 library(tidyverse)
+library(here)
 library(scales)
 library(countrycode)
-library(here)
+library(patchwork)
 
 # -------------------------------------------------
 ## Load data
@@ -51,15 +52,15 @@ top20_asylum_plot <- top20_asylum_overall %>%
     color = "black",
     alpha = 0.8
   ) +
-  labs(title = "Top 20 Asylum Countries From 2010-2022") +
+  labs(title = "Top 20 Asylum Countries") +
   scale_y_continuous(labels = label_number(suffix = "M", scale = 1e-6)) +
   coord_flip() +
   theme_minimal() +
-  theme(title = element_text(size = rel(1.25)),
-        plot.background = element_rect(fill = "white"),
+  theme(plot.title = element_text(size = rel(0.9), hjust = 0.5),
+        plot.background = element_rect(fill = "white", color = "white"),
         axis.title = element_blank())
 
-# Top 20 refugee countries-of-origin 2010-2022
+# Top 20 refugee countires of origin 2010-2022
 top20_coo_overall <- 
   summarize(
     population,
@@ -77,12 +78,12 @@ top20_coo_plot <- top20_coo_overall %>%
     color = "black",
     alpha = 0.8
   ) +
-  labs(title = "Top 20 Refugee Countries of Origin 2010-2022") +
+  labs(title = "Top 20 Countries of Origin") +
   scale_y_continuous(labels = label_number(suffix = "M", scale = 1e-6)) +
   coord_flip() +
   theme_minimal() +
-  theme(title = element_text(size = rel(1.25)),
-        plot.background = element_rect(fill = "white"),
+  theme(plot.title = element_text(size = rel(0.9), hjust = 0.5),
+        plot.background = element_rect(fill = "white", color = "white"),
         axis.title = element_blank())
   
   
@@ -117,22 +118,25 @@ world_map_plot <- ggplot() +
                linewidth = 0.2) +
     scale_fill_gradient(low = "royalblue", high = "gold",
                       labels = label_number(suffix = "M", scale = 1e-6),
-                      name = "Total Refugees") +
-  labs(title = "2010-2022 Refugees Accepted Per Country") +
+                      name = "Harbored Refugees") + 
   theme(
     panel.background = element_rect(fill = "white", color = "white"),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    title = element_text(size = rel(1.25)),
+    title = element_text(size = rel(0.9)),
     axis.text = element_blank(),
     axis.title = element_blank(),
     axis.ticks = element_blank()
   )
   
+# Combine plots
+plots_all <- (top20_asylum_plot | top20_coo_plot) / world_map_plot +
+  plot_annotation(
+    title = "Refugee Asylum and Countries of Origin (2010-2022)",
+    theme = theme(plot.title = element_text(face = "bold", hjust = 0.5, size = rel(1.5)))
+  )
 
 # -------------------------------------------------
 ## save plots
 
-# ggsave("top20_asylum_countries.png", top20_asylum_plot)
-# ggsave("top20_coo.png", top20_coo_plot)
-# ggsave("refugee_map.png", world_map_plot)
+# ggsave("tt_20230822_refugees.png", plots_all, width = 13, height = 10)
